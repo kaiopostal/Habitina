d495169313d2a125dd29c04d0a956353590d23cb
KaioPostal <83304473+kaiopostal@users.noreply.github.com>
2021-11-23 14:16:31 -0300

    Add files via upload

---
+++ Habitina Web (Responisvo)/script/script.js
@@ -0,0 +1,35 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
+const inputs = document.querySelectorAll(".input-field")
+
+function clicar() {
+
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+});
+}
+
+
+      
+
+
+
+
+
+
+
+
+
+
